98aa6f37571d3451429687a5d274b4feb1147a3d
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-06-30 20:07:05 -0700

    apps

---
+++ apps/todo/__init__.py
@@ -0,0 +1,57 @@
+import os
+from py4web import action, request, DAL, Field, Session, Cache, Condition
+
+# define session and cache objects
+session = Session()
+cache = Cache(size=1000)
+
+# define database and tables
+db = DAL(
+    "sqlite://storage.db", folder=os.path.join(os.path.dirname(__file__), "databases")
+)
+db.define_table("todo", Field("info"))
+db.commit()
+
+# an example of a custom requirement
+user_in_session = Condition(lambda: session.get('user', False))
+
+# example index page using session, template and vue.js
+@action("index")  # the function below is exposed as a GET action
+@action.uses("index.html", session)  # we use the template index.html and session
+def index():
+    session["counter"] = session.get("counter", 0) + 1
+    session["user"] = {"id": 1}  # store a user in session
+    return dict(session=session)
+
+
+# example of GET/POST/DELETE RESTful APIs
+
+
+@action("api", method="GET")  # a GET API function
+@action.uses(session, db)  # we load the session and db
+@action.uses(user_in_session)  # then check we have a valid user in session
+def todo():
+    return dict(items=db(db.todo).select(orderby=~db.todo.id).as_list())
+
+
+@action("api", method="POST")
+@action.uses(session, db)
+@action.uses(user_in_session)
+def todo():
+    return dict(id=db.todo.insert(info=request.json.get("info")))
+
+
+@action("api/<id:int>", method="DELETE")
+@action.uses(session, db)
+@action.uses(user_in_session)
+def todo(id):
+    db(db.todo.id == id).delete()
+    return dict()
+
+
+# example of caching
+@action("uuid")
+@cache.memoize(expiration=5)  # here we cache the result for 5 seconds
+def uuid():
+    import uuid
+    return str(uuid.uuid4())
---
+++ apps/todo/static/js/todo.js
@@ -0,0 +1,29 @@
+// initialize the app
+var app = {data:{}, methods:{}};
+// data exposed to the view
+app.api = '/' + window.location.href.split('/')[3] + '/api';
+app.data.items = [];
+app.data.input = '';
+// methods exposed to the view
+app.methods.edit = function(item_id) { };
+app.methods.remove = function(item_id) { 
+    Q.delete(app.api + '/' + item_id).then(function(){
+            app.vue.items=app.vue.items.filter(function(item){
+                    return item.id!=item_id;
+                }); 
+        });
+};
+app.methods.save = function(item_id) { 
+    var data = {};
+    data.info = app.vue.input;
+    Q.post(app.api, data).then(function(res){            
+            if (app.vue.input) app.vue.items.unshift({id:res.json().id, info: app.vue.input});
+            app.vue.input='';
+        });
+};
+
+// start the app
+app.vue = new Vue({el:"#vue", data: app.data, methods: app.methods});
+Q.get(app.api).then(function(res){
+        app.vue.items = res.json().items;
+    });
